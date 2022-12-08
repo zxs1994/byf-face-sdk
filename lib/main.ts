8024956fc826dbd60e363290e2607979c2a9de42
zxs1994 <695644578@qq.com>
2022-12-08 12:48:28 +0800

    ..

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,5 @@
 // import faceapi from 'face-api.js'
-import { defineCustomElement } from 'vue'
+import { defineCustomElement, VueElement } from 'vue'
 import ByfFaceSdk, { ByfFaceSdkProps } from './ByfFaceSdk.ce.vue'
 // 转换为自定义元素构造器
 const ByfFaceSdkElement = defineCustomElement(ByfFaceSdk)
@@ -7,7 +7,6 @@ const ByfFaceSdkElement = defineCustomElement(ByfFaceSdk)
 // 注册之后，所有此页面中的 `<my-vue-element>` 标签
 // 都会被升级
 customElements.define('byf-face-sdk', ByfFaceSdkElement)
-import { VueElement } from "vue"
 
 export declare interface ByfFaceInit {
   (box: HTMLDivElement, props: ByfFaceSdkProps): VueElement
